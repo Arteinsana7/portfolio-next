ddb61efa2844f2a4db037040a8da98ad52f28204
Arteinsana7 <147487242+Arteinsana7@users.noreply.github.com>
2024-07-16 16:37:39 +0200

    Bento Grid
    Bento grid to create sections on the projet.

---
+++ .history/app/page_20240716013852.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Hero from "./components/Hero";
+import Grid from "./components/Grid";
+
+import { FloatingNavbar } from "./ui/FloatingNavbar";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNavbar navItems={[{name:'Home', link:'/', icon:<FaHome />}
+      ]}/>
+        <Hero/>
+        <Grid />
+        
+        </div>
+
+    </main>
+  );
+}
---
+++ .history/components/Grid_20240716112838.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BentoGrid } from './ui/BentoGrid'
+const Grid = () => {
+  return (
+    <section id = "about">
+        <BentoGrid>
+      {[{}]}
+      </BentoGrid>
+    </section>
+  )
+}
+
+export default Grid
