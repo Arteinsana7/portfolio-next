1ed512da348ebc6f1fffd9afedaa86f27d17c805
Eliana. <e.arteinsana@gmail.com>
2026-09-18 18:21:30 +0200

    fixs

--- .history/components/Hero_20260918181826.tsx
+++ .history/components/Hero_20260918181826.tsx
@@ -38,7 +38,7 @@ const Hero = () => {
           <TextGenerateEffect
             words={"Bonjour, je suis Eliana,\nDesigneuse Digitale\net Développeuse créative"}
             mobileWords={"Bonjour,\nje suis Eliana,\nDesigneuse\nDigitale\net Développeuse\ncréative"}
-            className="text-center text-[40px] md:text-5xl lg:text-6xl mt-2 md:mt-0"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl mt-10 md:mt-0"
           />
 
           {/* 
